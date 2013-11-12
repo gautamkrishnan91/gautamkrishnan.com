97348fb2ed42033b90305d5903f44c12dac838d3
gkthegr8 <gkthegr8@gmail.com>
2013-11-12 23:24:02 +0530

    Modified header for responsive design

---
+++ js/responsive.js
@@ -0,0 +1,10 @@
+$(document).ready(function()
+{
+	$('#intro').height(window.innerHeight);
+	$('#projects').height(window.innerHeight);
+	$('#labs').height(window.innerHeight);
+	$('#about').height(window.innerHeight);
+});
+$('.mobilemenu-right').click(function(){
+	$( "#mobilemenu-dropdown" ).slideToggle();
+});
